7024efc16e8554b37103c26285cb3d47d2802249
tahadigihub <taharoshaan2007@gmail.com>
2024-12-16 05:26:34 -0800

    program07

---
+++ script.js
@@ -0,0 +1,28 @@
+{/* <script>var userInput = prompt("Enter a Number");
+if (userInput > 1) {
+    console.log("Invalid Input");
+} else if("userInput == 1"){
+    console.log("Bulb is on");
+}
+else if ("userInput == 0"){
+   console.log("Bulb is off");
+}
+else{
+    console.log("Invalid Input") */}
+// }
+//    </script>
+// var userInput = prompt("Enter a Number"); // Prompt user for input
+// userInput = Number(userInput); // Convert the input to a number
+
+// if (userInput > 1) {
+//     console.log("Invalid Input");
+// } else if (userInput === 1) {
+//     console.log("Bulb is on");
+// } else if (userInput === 0) {
+//     console.log("Bulb is off");
+// } else {
+//     console.log("Invalid Input");
+// }
+
+// var userInput = prompt("Enter a Number");
+// console.log("User entered:", userInput);
